e9c05e580384aa378880874760c666f6ba1fb3fe
openConnection <34420227+openConnection@users.noreply.github.com>
2019-01-31 19:05:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,31 +3,31 @@ import java.util.Random;
 
 public class Main {
 	
-	//Variables stockées
+	//Variables stockÃ©es
 	public static ArrayList<Integer> tableauPorte = new ArrayList<>(); //Tableau contenant les portes
-	public static Integer victoire; //Nombre représentant les victoires lorsque qu'on change de porte
+	public static Integer victoire; //Nombre reprÃ©sentant les victoires lorsque qu'on change de porte
 
 	public static void main(String[] args) {
 		
-		//Remplissage du tableau avec le numéro des portes
+		//Remplissage du tableau avec le numÃ©ro des portes
 		tableauPorte.add(1);
 		tableauPorte.add(2);
 		tableauPorte.add(3);
 		
-		Random rand = new Random(); //Initialisation d'une fonction mathématique permettant de générer un nombre
+		Random rand = new Random(); //Initialisation d'une fonction mathÃ©matique "alÃ©atoire" permettant de gÃ©nÃ©rer un nombre
 		
-		for(int i = 0; i < 1000; i++){ //Création d'une boucle qui exécutera le code 1000 fois
+		for(int i = 0; i < 1000; i++){ //CrÃ©ation d'une boucle qui exÃ©cutera le code 1000 fois
 			
 			Integer porte_gagnante = tableauPorte.get(rand.nextInt(tableauPorte.size())); //On nomme une porte gagnante au hasard
 			Integer porte_vue = tableauPorte.get(rand.nextInt(tableauPorte.size())); //On nomme la porte qu'on va montrer au hasard
 			Integer porte_choisie = tableauPorte.get(rand.nextInt(tableauPorte.size())); //On nomme la poret qu'on choisie au hasard
 			
 			do {
-				porte_vue = tableauPorte.get(rand.nextInt(tableauPorte.size())); //Exécution de la condition
-            } while ((porte_vue == porte_choisie) || (porte_vue == porte_gagnante)); //Si la porte montrée ou gagnante est la même que celle choisie, on la renomme au hasard
+				porte_vue = tableauPorte.get(rand.nextInt(tableauPorte.size())); //ExÃ©cution de la condition
+            } while ((porte_vue == porte_choisie) || (porte_vue == porte_gagnante)); //Si la porte montrÃ©e ou gagnante est la mÃªme que celle choisie, on la renomme au hasard
 			
 			Integer choix_2 = ((porte_vue + porte_choisie) != 4) ? (porte_vue + porte_choisie) %4 : (porte_vue + porte_choisie)/2;
-			// ? est une condition, ici en deuxième choix, on choisi la porte qui n'a pas été choisie
+			// ? est une condition, ici en deuxiÃ¨me choix, on choisi la porte qui n'a pas Ã©tÃ© choisie
 			if(choix_2 == porte_gagnante){
 				victoire++; // Si le choix 2 est la porte gagnante, on augmente de 1 la victoire par changement de porte
 			}
